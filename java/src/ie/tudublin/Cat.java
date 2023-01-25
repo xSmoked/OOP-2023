81b06abf4f605a90ac917cd7f9087c454a0c32e7
xSmoked <xsmoked@xSmokeds-MacBook-Pro.local>
2023-01-25 16:17:39 +0000

    killing the cat

---
+++ java/src/ie/tudublin/Cat.java
@@ -0,0 +1,23 @@
+package ie.tudublin;
+
+public class Cat {
+    private int nineLives = 9;
+    private String name;
+
+    public int getNumLives() {
+        return this.nineLives;
+    }
+    public Cat(String name){
+        this.name = name;
+    }
+
+    public void Kill(){
+        if(nineLives-- == 0){
+            System.out.println("Dead");
+        }
+        else {
+            System.out.println("Ouch");
+        }
+    }
+
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -4,11 +4,12 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
+		Cat ginger = new Cat("Ginger");
+		int numLives = ginger.getNumLives();
+		for(int i = 0; i < numLives+1; i++)
+		{
+			ginger.Kill();
+		}
 	}
 	
 }
