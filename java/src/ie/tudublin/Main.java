233d271a35f6ce09323107d0e00055e3d542296d
xSmoked <xsmoked@xSmokeds-MacBook-Pro.local>
2023-01-30 11:32:30 +0000

    Monday 2 Class

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -11,11 +11,6 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
 
 		helloProcessing();
 	}
